bb929f233ca14e4c4f2655e8354f50fad36a1eb0
Richa Luhtra <richaluhtra.ca@gmail.com>
2020-09-09 05:38:00 -0700

    first commit

---
+++ src/com/richa/Main.java
@@ -0,0 +1,62 @@
+package com.richa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      /*  boolean isCanadian = false;
+        System.out.println(isCanadian);
+        char ch='d';
+        System.out.println(ch);
+         ch=100;
+        System.out.println(ch);
+        ch=255;
+        System.out.println(ch);
+        ch=256;
+        System.out.println(ch);
+        ch=2727;
+        System.out.println(ch);
+        byte chunk='c';
+        System.out.println(chunk);
+        chunk=127;
+        System.out.println(chunk);
+         double n1=312.3245;
+         double n2=23.54234;
+        double result;
+        result=n1+n2;
+        System.out.println(result);
+        result =n1-n2;
+        System.out.println(result);
+        result =n1*n2;
+        System.out.println(result);
+        result =n1/n2;
+        System.out.println(result);
+        System.out.println(n1/n2);
+        */
+
+        int a=2;
+        int sum=a++ +a+ a++;
+        System.out.println(sum);
+        a=2;
+        sum=++a +a+ ++a;
+        System.out.println(sum);
+        /*
+
+        char ch='a';
+        ch++;
+        System.out.println(ch);
+
+
+int a=9;
+        int b=8;
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println(a<=b);
+
+        System.out.println(a>=b);
+    */
+    }
+
+
+        }
---
+++ src/com/richa/StringDemo.java
@@ -0,0 +1,11 @@
+package com.richa;
+
+public class StringDemo {
+    public static void main(String[] args) {
+       int a=20;
+       int b=30;
+       if(a>b)
+           System.out.println(a);
+
+    }
+}
